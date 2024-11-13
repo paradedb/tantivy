03cc2efa523a2ce81445b1f84709a580c92fcaaa
Ming Ying <ming.ying.nyc@gmail.com>
2024-11-13 10:39:23 -0500

    Directory trait can read/write meta/managed
    (cherry picked from commit 02141a4bd453d1e5f6b226d0fbf9d9f91db64bad)

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -13,8 +13,10 @@ mod segment_reader;
 
 pub(crate) use self::index::{builtin_plugins, list_segment_files};
 pub use self::index::{Index, IndexBuilder};
-pub(crate) use self::index_meta::SegmentMetaInventory;
-pub use self::index_meta::{IndexMeta, IndexSettings, IndexSortByField, Order, SegmentMeta};
+pub use self::index_meta::{
+    IndexMeta, IndexSettings, IndexSortByField, InnerSegmentMeta, Order, SegmentMeta,
+    SegmentMetaInventory,
+};
 pub use self::inverted_index_plugin::{InvertedIndexPlugin, InvertedIndexPluginWriter};
 pub use self::inverted_index_reader::InvertedIndexReader;
 pub use self::segment::Segment;
