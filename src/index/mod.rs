24180dea5fbc3c30bd889c4fb4d9c00d2b85bc1b
Ming Ying <ming.ying.nyc@gmail.com>
2024-12-13 09:50:19 -0500

    small changes to make MVCC work with delete
    (cherry picked from commit 9055888d7e59eb0704d4b2703eba59b14f2900fb)

--- src/index/mod.rs
+++ src/index/mod.rs
@@ -14,7 +14,7 @@ mod segment_reader;
 pub(crate) use self::index::{builtin_plugins, list_segment_files};
 pub use self::index::{Index, IndexBuilder};
 pub use self::index_meta::{
-    IndexMeta, IndexSettings, IndexSortByField, InnerSegmentMeta, Order, SegmentMeta,
+    DeleteMeta, IndexMeta, IndexSettings, IndexSortByField, InnerSegmentMeta, Order, SegmentMeta,
     SegmentMetaInventory,
 };
 pub use self::inverted_index_plugin::{InvertedIndexPlugin, InvertedIndexPluginWriter};
