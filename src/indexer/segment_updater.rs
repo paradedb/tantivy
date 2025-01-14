3fe2005a24c0ab60bd507ecfd06f2f96c85a29a9
Neil Hansen <neil.hansen.31@gmail.com>
2025-01-13 20:11:44 -0800

    run cargo fmt

--- src/indexer/segment_updater.rs
+++ src/indexer/segment_updater.rs
@@ -360,8 +360,8 @@ impl SegmentUpdater {
         let segment_updater = self.clone();
         self.schedule_task(move || {
             segment_updater.segment_manager.add_segment(segment_entry);
-            // mingy98: We don't need to consider merge options for every segment, just at the very end
-            // segment_updater.consider_merge_options();
+            // mingy98: We don't need to consider merge options for every segment, just at the very
+            // end segment_updater.consider_merge_options();
             Ok(())
         })
     }
