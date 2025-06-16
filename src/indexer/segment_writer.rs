14fe7db6f407799e5eca4967e07715dd15a0e0e5
Ming Ying <ming.ying.nyc@gmail.com>
2025-06-15 21:50:34 -0400

    segmentwriter can ignore store

--- src/indexer/segment_writer.rs
+++ src/indexer/segment_writer.rs
@@ -73,6 +73,7 @@ pub struct SegmentWriter {
     per_field_text_analyzers: Vec<TextAnalyzer>,
     term_buffer: Term,
     schema: Schema,
+    ignore_store: bool,
 }
 
 impl SegmentWriter {
@@ -85,7 +86,11 @@ impl SegmentWriter {
     ///   behavior as a memory limit.
     /// - segment: The segment being written
     /// - schema
-    pub fn for_segment(memory_budget_in_bytes: usize, segment: Segment) -> crate::Result<Self> {
+    pub fn for_segment(
+        memory_budget_in_bytes: usize,
+        segment: Segment,
+        ignore_store: bool,
+    ) -> crate::Result<Self> {
         let schema = segment.schema();
         let tokenizer_manager = segment.index().tokenizers().clone();
         let tokenizer_manager_fast_field = segment.index().fast_field_tokenizer().clone();
@@ -130,6 +135,7 @@ impl SegmentWriter {
             per_field_text_analyzers,
             term_buffer: Term::with_capacity(16),
             schema,
+            ignore_store,
         })
     }
 
@@ -156,6 +162,7 @@ impl SegmentWriter {
             &self.fieldnorms_writer,
             self.segment_serializer,
             mapping.as_ref(),
+            self.ignore_store,
         )?;
         let doc_opstamps = remap_doc_opstamps(self.doc_opstamps, mapping.as_ref());
         Ok(doc_opstamps)
@@ -420,6 +427,7 @@ fn remap_and_write(
     fieldnorms_writer: &FieldNormsWriter,
     mut serializer: SegmentSerializer,
     doc_id_map: Option<&DocIdMapping>,
+    ignore_store: bool,
 ) -> crate::Result<()> {
     debug!("remap-and-write");
     if let Some(fieldnorms_serializer) = serializer.extract_fieldnorms_serializer() {
@@ -429,7 +437,6 @@ fn remap_and_write(
         .segment()
         .open_read(SegmentComponent::FieldNorms)?;
     let fieldnorm_readers = FieldNormReaders::open(fieldnorm_data)?;
-    let uses_store = schema.fields().any(|(_, entry)| entry.is_stored());
     serialize_postings(
         ctx,
         schema,
@@ -442,7 +449,7 @@ fn remap_and_write(
     fast_field_writers.serialize(serializer.get_fast_field_write(), doc_id_map)?;
 
     // finalize temp docstore and create version, which reflects the doc_id_map
-    if uses_store {
+    if !ignore_store {
         if let Some(doc_id_map) = doc_id_map {
             debug!("resort-docstore");
             let store_write = serializer
