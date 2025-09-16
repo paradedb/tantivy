895e6764b3c8cd7b9e720380ebcbaf82a6853195
Stu Hood <stuhood@gmail.com>
2025-09-15 17:25:19 -0700

    WIP

--- src/directory/footer.rs
+++ src/directory/footer.rs
@@ -33,6 +33,7 @@ impl Footer {
         self.crc
     }
     pub fn append_footer<W: io::Write>(&self, write: &mut W) -> io::Result<()> {
+        println!(">>> appending footer {self:?}");
         // 24 bytes
         BinarySerializable::serialize(&self.version.major, write)?;
         BinarySerializable::serialize(&self.version.minor, write)?;
