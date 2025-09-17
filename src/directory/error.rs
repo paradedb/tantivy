897d324b6145a2e5cf4675af01072632cb551e9d
Stu Hood <stuhood@gmail.com>
2025-09-17 09:39:37 -0700

    WIP

--- src/directory/error.rs
+++ src/directory/error.rs
@@ -154,6 +154,8 @@ pub enum OpenReadError {
     /// The file does not exist.
     #[error("Files does not exist: {0:?}")]
     FileDoesNotExist(PathBuf),
+    #[error("File is corrupt: {0:?}")]
+    Corrupt(PathBuf),
     /// Any kind of io::Error.
     #[error(
         "IoError: '{io_error:?}' happened while opening the following file for Read: {filepath}."
