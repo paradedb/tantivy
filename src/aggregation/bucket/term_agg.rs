f25b1023881cffc25f93077dba3e474b73db54b9
rjhallsted <rjhallsted@gmail.com>
2026-04-17 16:23:19 -0600

    Clean up null bitset unit tests into a single test

--- src/aggregation/bucket/term_agg.rs
+++ src/aggregation/bucket/term_agg.rs
@@ -146,7 +146,9 @@ pub enum IncludeExcludeParam {
 
 impl Serialize for IncludeExcludeParam {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where S: serde::Serializer {
+    where
+        S: serde::Serializer,
+    {
         match self {
             IncludeExcludeParam::Regex(s) => serializer.serialize_str(s),
             IncludeExcludeParam::Values(v) => v.serialize(serializer),
@@ -157,7 +159,9 @@ impl Serialize for IncludeExcludeParam {
 // Custom deserializer to accept either a single string (regex) or an array of strings (values).
 impl<'de> Deserialize<'de> for IncludeExcludeParam {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where D: serde::Deserializer<'de> {
+    where
+        D: serde::Deserializer<'de>,
+    {
         use serde::de::{self, SeqAccess, Visitor};
         struct IncludeExcludeVisitor;
 
@@ -169,22 +173,30 @@ impl<'de> Deserialize<'de> for IncludeExcludeParam {
             }
 
             fn visit_str<E>(self, v: &str) -> Result<Self::Value, E>
-            where E: de::Error {
+            where
+                E: de::Error,
+            {
                 Ok(IncludeExcludeParam::Regex(v.to_string()))
             }
 
             fn visit_borrowed_str<E>(self, v: &'de str) -> Result<Self::Value, E>
-            where E: de::Error {
+            where
+                E: de::Error,
+            {
                 Ok(IncludeExcludeParam::Regex(v.to_string()))
             }
 
             fn visit_string<E>(self, v: String) -> Result<Self::Value, E>
-            where E: de::Error {
+            where
+                E: de::Error,
+            {
                 Ok(IncludeExcludeParam::Regex(v))
             }
 
             fn visit_seq<A>(self, mut seq: A) -> Result<Self::Value, A::Error>
-            where A: SeqAccess<'de> {
+            where
+                A: SeqAccess<'de>,
+            {
                 let mut values: Vec<String> = Vec::new();
                 while let Some(elem) = seq.next_element::<String>()? {
                     values.push(elem);
@@ -2960,102 +2972,17 @@ mod tests {
     }
 
     #[test]
-    fn null_bitset_bounds_check_regresiion_include_0() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(0)?;
-        let agg_req = agg_terms_request_for_regression_check(false)?;
+    fn null_bitset_bounds_check_regression() -> crate::Result<()> {
+        // include cases
+        for i in 0..=4 {
+            let index = prep_index_with_n_unique_terms_plus_one_null(i * 64)?;
+            let include_req = agg_terms_request_for_regression_check(false)?;
+            let exclude_req = agg_terms_request_for_regression_check(true)?;
 
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_include_64() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(64)?;
-        let agg_req = agg_terms_request_for_regression_check(false)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_include_128() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(128)?;
-        let agg_req = agg_terms_request_for_regression_check(false)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_include_192() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(192)?;
-        let agg_req = agg_terms_request_for_regression_check(false)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_include_256() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(256)?;
-        let agg_req = agg_terms_request_for_regression_check(false)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_exclude_0() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(0)?;
-        let agg_req = agg_terms_request_for_regression_check(true)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_exclude_64() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(64)?;
-        let agg_req = agg_terms_request_for_regression_check(true)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_exclude_128() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(128)?;
-        let agg_req = agg_terms_request_for_regression_check(true)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_exclude_192() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(192)?;
-        let agg_req = agg_terms_request_for_regression_check(true)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
-        Ok(())
-    }
-
-    #[test]
-    fn null_bitset_bounds_check_regresiion_exclude_256() -> crate::Result<()> {
-        let index = prep_index_with_n_unique_terms_plus_one_null(256)?;
-        let agg_req = agg_terms_request_for_regression_check(true)?;
-
-        // this should work and not panic
-        let _res = exec_request(agg_req, &index)?;
+            // this should work and not panic
+            let _res = exec_request(include_req, &index)?;
+            let _res = exec_request(exclude_req, &index)?;
+        }
         Ok(())
     }
 }
