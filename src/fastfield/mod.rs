69d90bfc6cb5af217f27830267bf92342eee0305
James Sewell <james.sewell@gmail.com>
2026-05-02 06:02:55 +1200

    refactor: extract codec selection mechanics (#136)
    Add the ability to configure which columnar codecs are used via
    IndexSettings, and thread that configuration through serialization,
    segment writing, and merge paths.
    
    (cherry picked from commit 6fb61e527a8923bfde80684eb8345a7266a13d9b)

--- src/fastfield/mod.rs
+++ src/fastfield/mod.rs
@@ -127,7 +127,9 @@ mod tests {
             fast_field_writers
                 .add_document(&doc!(*FIELD=>2u64))
                 .unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -178,7 +180,9 @@ mod tests {
             fast_field_writers
                 .add_document(&doc!(*FIELD=>215u64))
                 .unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -211,7 +215,9 @@ mod tests {
                     .add_document(&doc!(*FIELD=>100_000u64))
                     .unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -243,7 +249,9 @@ mod tests {
                     .add_document(&doc!(*FIELD=>5_000_000_000_000_000_000u64 + doc_id))
                     .unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -276,7 +284,9 @@ mod tests {
                 doc.add_i64(i64_field, i);
                 fast_field_writers.add_document(&doc).unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -315,7 +325,9 @@ mod tests {
             let mut fast_field_writers = FastFieldsWriter::from_schema(&schema).unwrap();
             let doc = TantivyDocument::default();
             fast_field_writers.add_document(&doc).unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
 
@@ -348,7 +360,9 @@ mod tests {
             let mut fast_field_writers = FastFieldsWriter::from_schema(&schema).unwrap();
             let doc = TantivyDocument::default();
             fast_field_writers.add_document(&doc).unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
 
@@ -385,7 +399,9 @@ mod tests {
             for &x in &permutation {
                 fast_field_writers.add_document(&doc!(*FIELD=>x)).unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -770,7 +786,9 @@ mod tests {
             fast_field_writers
                 .add_document(&doc!(field=>false))
                 .unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -802,7 +820,9 @@ mod tests {
                     .add_document(&doc!(field=>false))
                     .unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -827,7 +847,9 @@ mod tests {
             let mut fast_field_writers = FastFieldsWriter::from_schema(&schema).unwrap();
             let doc = TantivyDocument::default();
             fast_field_writers.add_document(&doc).unwrap();
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         let file = directory.open_read(path).unwrap();
@@ -855,7 +877,9 @@ mod tests {
             for doc in docs {
                 fast_field_writers.add_document(doc).unwrap();
             }
-            fast_field_writers.serialize(&mut write, None).unwrap();
+            fast_field_writers
+                .serialize(&columnar::DEFAULT_CODEC_TYPES, &mut write, None)
+                .unwrap();
             write.terminate().unwrap();
         }
         Ok(directory)
