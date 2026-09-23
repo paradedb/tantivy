75e0675a9fc7e91e3d4d96126391479482258a52
Ming Ying <ming.ying.nyc@gmail.com>
2026-09-23 01:02:27 -0700

    perf: remove diagnostic counters from posting norm lookups

--- src/postings/mod.rs
+++ src/postings/mod.rs
@@ -23,7 +23,9 @@ pub(crate) mod term_norms;
 
 pub use loaded_postings::LoadedPostings;
 pub(crate) use stacker::compute_table_memory_size;
-pub use term_norms::{posting_norm_reads, set_posting_norms_enabled};
+#[cfg(test)]
+pub use term_norms::posting_norm_reads;
+pub use term_norms::set_posting_norms_enabled;
 
 pub use self::block_segment_postings::BlockSegmentPostings;
 pub(crate) use self::indexing_context::IndexingContext;
